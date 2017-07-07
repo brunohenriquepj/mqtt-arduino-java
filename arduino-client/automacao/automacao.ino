16860648cf3b9261f6da7ce04a1d78b8851e06f8
Bruno Pereira <bruno.henriquepj@gmail.com>
2017-07-06 23:07:27 -0300

    Adiciona função para subscribe de tópicos.

--- arduino-client/automacao/automacao.ino
+++ arduino-client/automacao/automacao.ino
@@ -41,6 +41,7 @@ PubSubClient client(ethClient);
 void conecta_cliente();
 void callback(char* topic, byte* payload, unsigned int length);
 void imprime_erro_de_conexao_mqtt(int estado);
+void subscribe_topicos();
 
 void setup() {
     Serial.begin(9600);
@@ -65,6 +66,7 @@ void conecta_cliente() {
         Serial.println("MQTT - Tentativa de conexao com broker...");
         if (client.connect(CLIENT_ID, CLIENTE_USUARIO, CLIENTE_SENHA)) {
             Serial.println("Cliente conectado.");
+            subscribe_topicos();
         }
         else {
             imprime_erro_de_conexao_mqtt(client.state());
@@ -88,4 +90,23 @@ void callback(char* topic, byte* payload, unsigned int length) {
 void imprime_erro_de_conexao_mqtt(int estado) {
     Serial.print("ERRO AO CONECTAR CLIENTE. Causa: ");
     Serial.println(estado); // http://pubsubclient.knolleary.net/api.html#state
+}
+
+void subscribe_topicos() {
+    Serial.println("Inscrevendo em topicos.");
+    if (client.connected()) {
+        // AREA 1
+        client.subscribe(LAMPADA1_AREA1_ESTADO_TOPICO_SUB, QOS);
+        client.subscribe(LAMPADA1_AREA1_VALOR_TOPICO_SUB, QOS);
+        client.subscribe(LAMPADA2_AREA1_ESTADO_TOPICO_SUB, QOS);
+        client.subscribe(LAMPADA2_AREA1_VALOR_TOPICO_SUB, QOS);
+        // AREA 2
+        client.subscribe(LAMPADA1_AREA2_ESTADO_TOPICO_SUB, QOS);
+        client.subscribe(LAMPADA1_AREA2_VALOR_TOPICO_SUB, QOS);
+        client.subscribe(LAMPADA2_AREA2_ESTADO_TOPICO_SUB, QOS);
+        client.subscribe(LAMPADA2_AREA2_VALOR_TOPICO_SUB, QOS);
+
+        // Atualiza valores
+        client.subscribe(ATUALIZA_VALORES_TOPICO_SUB, QOS);
+    }
 }
