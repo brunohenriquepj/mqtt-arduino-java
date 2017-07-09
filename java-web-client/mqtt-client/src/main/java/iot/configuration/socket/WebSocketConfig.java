be7b143a6cc77afd05a71a98c80327db51cde049
Bruno Pereira <bruno.henriquepj@gmail.com>
2017-07-09 20:20:25 -0300

    Adiciona configuração do webSocket

---
+++ java-web-client/mqtt-client/src/main/java/iot/configuration/socket/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package iot.configuration.socket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+/**
+ * Created by bruno on 09/07/17.
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/server-iot-websocket").withSockJS();
+    }
+
+}
